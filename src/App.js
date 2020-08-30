fe131911a8c938ddc80181e2fda3bab2e9a6a465
andre <andreantoine0016@gmail.com>
2020-08-31 01:12:49 +0200

    rows and layout

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
     <h1>netflix front end</h1>
     <Row title="Most watched" fetchUrl={requests.fetchMostwatched}/>
     <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated" fetchUrl={requests.fetchToprated}/>
+    <Row title="Action movies" fetchUrl={requests.fetchActionmovies}/>
+    <Row title="Comedy movies" fetchUrl={requests.fetchComedymovies}/>
+    <Row title="Horror movies" fetchUrl={requests.fetchHorrormovies}/>
+    <Row title="Romance movies" fetchUrl={requests.fetchRomancemovies}/>
+    <Row title="Documentaries" fetchUrl={requests.fetchDocumentries}/>
 
     </div>
   );
